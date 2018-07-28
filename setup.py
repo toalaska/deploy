16c1eaab518536686ffd54c198a1e01c7214b039
sgzh <sg@toalaska.net>
2018-07-28 17:32:37 +0800

    -

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+setup(name="deploy",  # 项目名
+      version="0.0.1",       # 版本号
+      description="项目部署工具",  #简介
+      long_description=long_description,  # 长简介 这里使用的 readme 内容
+      long_description_content_type="text/markdown",
+      license="BSD",   # 授权
+      install_requires=requirements, # 依赖
+      author="toalaska",  # 作者
+      author_email="toalaska@126.com",  # 邮箱
+      url="https://github.com/toalaska/deploy",  # 地址
+      download_url="https://github.com/toalaska/deploy/archive/master.zip",
+      packages=find_packages(),
+      keywords=["deploy", "oss","ssh"],
+      zip_safe=True)
