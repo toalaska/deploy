9317e49e1d7f363a93e0b1e6481d1e8b7595ebed
sgzh <sg@toalaska.net>
2018-07-28 18:16:15 +0800

    -

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as f:
     requirements = [l for l in f.read().splitlines() if l]
 
-setup(name="deploy",  # 项目名
+setup(name="deper",  # 项目名
       version="0.0.1",       # 版本号
       description="项目部署工具",  #简介
       long_description=long_description,  # 长简介 这里使用的 readme 内容
@@ -19,6 +19,6 @@ setup(name="deploy",  # 项目名
       author_email="toalaska@126.com",  # 邮箱
       url="https://github.com/toalaska/deploy",  # 地址
       download_url="https://github.com/toalaska/deploy/archive/master.zip",
-      packages=['deploy'],
+      packages=find_packages("deploy"),
       keywords=["deploy", "oss","ssh"],
       zip_safe=True)
